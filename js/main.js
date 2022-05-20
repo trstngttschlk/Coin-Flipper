1d20541e704e9da1cd6408674aee6d0977c49f9a
Tristan Gottschalk <trstn@gttschlk.com>
2022-05-20 12:24:11 -0400

    add counter of results in DOM

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
+// event listener for click on flip button
 document.querySelector('button').addEventListener('click',flipCoin)
-
+// variables for elements in DOM
 let headCount = document.querySelector('#headCount')
 let tailsCount = document.querySelector('#tailsCount')
 let resultText = document.querySelector('#flipResult')
-
-
-if (resultText == 'HEADS'){
-
-}
+// overview of each outcome
+let headInt = 0
+let tailsInt = 0
+// displays results overview in DOM
+headCount.innerText = headInt
+tailsCount.innerText = tailsInt
 
 function flipCoin(){
     let flip = Math.random()
     let result
     if (flip < .5){
         result = 'HEADS'
+        headInt += 1
+        headCount.innerText = headInt
     } else {
         result = 'TAILS'
+        tailsInt += 1
+        tailsCount.innerText = tailsInt
     }
     resultText.innerText = result
 }
-
